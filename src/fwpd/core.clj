7cb65e4d53606cdd82238f9e7bb8a4fa9a60738f
romankrru <rm07ru@gmail.com>
2018-06-17 21:58:24 +0300

    Add append

--- src/fwpd/core.clj
+++ src/fwpd/core.clj
@@ -35,7 +35,41 @@
 (defn glitter-filter
   [min-glitter records]
   (filter
-          #(>= (:glitter-index %) min-glitter)
-          recors))
+   #(>= (:glitter-index %) min-glitter)
+   records))
+
+(defn glitter-names
+  [records]
+  (map :name records))
 
 (glitter-filter 5 (mapify (parse (slurp filename))))
+(glitter-names (glitter-filter 5 (mapify (parse (slurp filename)))))
+
+(def not-blank? (complement clojure.string/blank?))
+
+(def validators {:name [not-blank?]
+                 :glitter-index [number?]})
+
+(defn validate
+  [record validators]
+  (let [validated (map (fn
+                         [[validator-key  validator-rules]]
+                         (map (fn [rule] (rule (validator-key record))) validator-rules))
+                       validators)]
+    (every? #(every? true? %) validated)))
+
+(validate {:name "some name"
+           :glitter-index 123}
+          validators)
+
+(defn append
+  "Append name and glitter-index to CSV"
+  [name glitter-index]
+  (let [is-valid (validate {:name name
+                            :glitter-index glitter-index}
+                           validators)]
+    (if is-valid
+      (spit filename (str name "," glitter-index "\n") :append true)
+      (println "Input is not valid."))))
+
+(append "new name" "2")
